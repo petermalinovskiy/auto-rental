aabd117bbefbe6b343397b36f826240d4b26cb09
Peter Malinovskiy <petermalinovskiy@gmail.com>
2020-06-01 13:59:18 +0300

    feat: add gulpfile and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const {src, dest, watch} = require ('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require ('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+// Static server
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./src/"
+      }
+  });
+  watch("./src/*.html").on('change', browserSync.reload);
+  watch("./src/sass/**/*.sass", serveSass);
+  watch("./src/sass/**/*.scss", serveSass);
+  watch("./src/js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./src/sass/**/*.sass")
+      .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+    }))
+      .pipe(dest("./src/css"))
+      .pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;
